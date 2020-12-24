74d533c3d54061cee45f48261097466548639ec7
sisa <cipotex@hotmail.com>
2020-12-24 13:15:22 -0600

    reportes excel.

--- iva_spring/src/main/java/innotech/com/configuration/PropertiesSource.java
+++ iva_spring/src/main/java/innotech/com/configuration/PropertiesSource.java
@@ -6,12 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration	
 @PropertySource("classpath:configuraciones.properties")
-public class PropertiesSource implements WebMvcConfigurer{
+public class PropertiesSource {
 
 	private final Logger log = LoggerFactory.getLogger(getClass());
 	
